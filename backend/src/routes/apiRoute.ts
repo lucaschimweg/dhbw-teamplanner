bfe02d75b65c453d685e4bca0f49ce010ca45b1c
lucaschimweg <l.schimweg@gmail.com>
2019-07-23 18:33:55 +0200

    added job dependency management

--- backend/src/routes/apiRoute.ts
+++ backend/src/routes/apiRoute.ts
@@ -229,6 +229,121 @@ export class ApiRoute {
         res.end();
     }
 
+    private async addJobDependency(req: express.Request, res: express.Response) {
+        if (!("parent" in req.body && "job" in req.body)) {
+            res.status(400).end("Bad Request");
+            return;
+        }
+
+        let parentId = req.body.parent;
+        let jobId = req.body.job;
+
+        let team = await Database.getInstance().getTeamById(req.user.teamId);
+        if (!team) {
+            res.status(500).end("Server error");
+            return;
+        }
+
+        if (team.leader.id != req.user.id) {
+            res.status(403).end("Forbidden");
+            return;
+        }
+
+        let parent = await Database.getInstance().getJobById(jobId);
+
+        if (!parent) {
+            res.status(400).end("Bad Request");
+            return;
+        }
+
+        if (parent.teamId != team.id) {
+            res.status(403).end("Forbidden");
+            return;
+        }
+
+        let job = await Database.getInstance().getJobById(jobId);
+
+        if (!job) {
+            res.status(400).end("Bad Request");
+            return;
+        }
+
+        if (job.teamId != team.id) {
+            res.status(400).end("Bad Request");
+            return;
+        }
+
+        let parents = await Database.getInstance().getParentJobs(jobId);
+        if (parents.indexOf(parentId) != -1) {  // Parent already parent of job
+            res.status(400).end("Bad Request");
+            return;
+        }
+
+        await Database.getInstance().addJobDependency(parentId, jobId);
+
+        await new JobScheduler(req.user.teamId).scheduleJobs(team.start);
+
+        res.writeHead(303, {
+            'Location': "/team"
+        });
+        res.end();
+    }
+
+    private async deleteJobDependency(req: express.Request, res: express.Response) {
+        if (!("parent" in req.body && "job" in req.body)) {
+            res.status(400).end("Bad Request");
+            return;
+        }
+
+        let parentId = req.body.parent;
+        let jobId = req.body.job;
+
+        let team = await Database.getInstance().getTeamById(req.user.teamId);
+        if (!team) {
+            res.status(500).end("Server error");
+            return;
+        }
+
+        if (team.leader.id != req.user.id) {
+            res.status(403).end("Forbidden");
+            return;
+        }
+
+        let parent = await Database.getInstance().getJobById(jobId);
+
+        if (!parent) {
+            res.status(400).end("Bad Request");
+            return;
+        }
+
+        if (parent.teamId != team.id) {
+            res.status(403).end("Forbidden");
+            return;
+        }
+
+        let job = await Database.getInstance().getJobById(jobId);
+
+        if (!job) {
+            res.status(400).end("Bad Request");
+            return;
+        }
+
+        if (job.teamId != team.id) {
+            res.status(400).end("Bad Request");
+            return;
+        }
+
+        // if parent does not exist in job nothing will happen
+        await Database.getInstance().deleteJobDependency(parentId, jobId);
+
+        await new JobScheduler(req.user.teamId).scheduleJobs(team.start);
+
+        res.writeHead(303, {
+            'Location': "/team"
+        });
+        res.end();
+    }
+
 
     constructor() {
         this.router.get("/doLogout", this.doLogout.bind(this));
@@ -237,7 +352,8 @@ export class ApiRoute {
         this.router.post("/deleteTeamUser", this.deleteTeamUser.bind(this));
         this.router.post("/addJobUser", this.addJobUser.bind(this));
         this.router.post("/deleteJobUser", this.deleteJobUser.bind(this));
-
+        this.router.post("/addJobDependency", this.addJobDependency.bind(this));
+        this.router.post("/deleteJobDependency", this.deleteJobDependency.bind(this));
     }
 
     public getRouter(): express.Router {
